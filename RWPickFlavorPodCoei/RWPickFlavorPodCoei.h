82133676c16f82002aac26915bc3d8c18b501d82
Gabriela Coelho <gabriela.coelho@Coelhos-Mac.local>
2018-11-29 09:34:33 -0200

    Inital Commit

---
+++ RWPickFlavorPodCoei/RWPickFlavorPodCoei.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavorPodCoei.h
+//  RWPickFlavorPodCoei
+//
+//  Created by Gabriela Coelho on 29/11/18.
+//  Copyright © 2018 Gabriela Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavorPodCoei.
+FOUNDATION_EXPORT double RWPickFlavorPodCoeiVersionNumber;
+
+//! Project version string for RWPickFlavorPodCoei.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorPodCoeiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavorPodCoei/PublicHeader.h>
+
+
